1441a852ec379a3e2aee2c4ed90a428f641d0ce9
NatiqJavid <Natiqj9@icloud.com>
2014-02-20 22:50:59 +0100

    Lille start

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test{
 
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		System.out.println("Hello World\n
+		 Hello everybody! have a nice day fuckers!");
 	}
 }
